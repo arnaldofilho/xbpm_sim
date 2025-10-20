Blademask:  - pixel value normalization;  - blade edges rounding value;  - renamed lines and columns variables to avoid mistakes between coordinate    system and array indexing;  - corrected and simplified pixel area analysis.
xbpm_sim:

 - added feature for uploading simulated image;
 - corrected calculation of boundaries of blades' window not to cut off the beam image at its edges;
 - included output recording of fluxes on blades for further analysis;

.gitignore:
 - numpy array files resulting from beam simulation (spectra).

blademask.py
@@ -19,7 +19,7 @@ class BladeMask:
 
     The standard values are based on real dimensions of the structures, in mm.
 
-    Version: 2025-07-22.
+    Version: 2025-08-14.
     """
     def __init__(self, gprm):
         """Define the main parameters of the mask."""
@@ -51,14 +51,17 @@ class BladeMask:
         # Create mask array with zeros.
         self.mask = np.zeros((self.nbins[0], self.nbins[1]))
 
+        # Scale to array units.
+        pxnorm = 1. / self.pixelsize
+
         # Run through blades to assign weights to pixels.
         for ib, blade in enumerate(self.bladescoordinates):
             # Equations of lines joining the corners of the blade.
             bladeequations = [
-                self._edge_line(blade[0], blade[1]),
-                self._edge_line(blade[1], blade[2]),
-                self._edge_line(blade[2], blade[3]),
-                self._edge_line(blade[3], blade[0]),
+                self._edge_line(blade[0] * pxnorm, blade[1] * pxnorm),
+                self._edge_line(blade[1] * pxnorm, blade[2] * pxnorm),
+                self._edge_line(blade[2] * pxnorm, blade[3] * pxnorm),
+                self._edge_line(blade[3] * pxnorm, blade[0] * pxnorm),
             ]
 
             # Set horizontal (x) interval, and top and bottom line
@@ -92,9 +95,9 @@ class BladeMask:
             # cross each other inside the pixel.
             self._pixel_corner_weight(blade, bladeequations)
 
-            # Run vertically in each interval to assign weight to pixels.
-            # Treat bulk and borders of the blades, except for corners.
-            for ii, interval in enumerate(intervals):
+            # Run vertically in each interval to assign weight to pixels:
+            # treat bulk and borders of the blades, except corners.
+            for jj, interval in enumerate(intervals):
                 # Define the interval corresponding to current blade. Take the
                 # limits of the box, [0, boxsize[0/1]], into consideration.
                 # xA, xB = max(interval[0], 0), min(interval[1], self.boxsize)
@@ -104,38 +107,34 @@ class BladeMask:
                 # physical, to guarantee each pixel will be analysed only once.
                 # Nx = int((xB - xA) / pixelsize)  # Number of intervals
                 # (round up).
-                ncolmin = max(0, int(interval[0] / self.pixelsize))
-                ncolmax = min(int(interval[1] / self.pixelsize) + 1,
-                              self.nbins[1] - 1)
+                ncolmin = max(0, round(interval[0] * pxnorm))
+                ncolmax = min(round(interval[1] * pxnorm), self.nbins[1])
 
-                # Run over all pixels.
+                # Run over all pixels inside intervals defined by
+                # blade's corners.
                 # Horizontal range to be scanned.
                 for ncol in range(ncolmin, ncolmax):
-                    # Vertical limits in physical (real) coordinates.
-                    rx = ncol * self.pixelsize
+                    # Vertical limits.
                     ymin = min(
-                        self._linear(rx, *bot_eq[ii]),
-                        self._linear(rx + self.pixelsize, *bot_eq[ii]),
+                        self._linear(ncol, *bot_eq[jj]),
+                        self._linear(ncol + 1, *bot_eq[jj]),
                     )
                     ymax = max(
-                        self._linear(rx, *top_eq[ii]),
-                        self._linear(rx + self.pixelsize, *top_eq[ii]),
+                        self._linear(ncol, *top_eq[jj]),
+                        self._linear(ncol + 1, *top_eq[jj]),
                     )
                     # Limits in array coordinates.
-                    nlinmin, nlinmax = (
-                        round(ymin / self.pixelsize),
-                        round(ymax / self.pixelsize + 1),
-                    )
-                    # int(ymin / self.pixelsize),
-                    # int(ymax / self.pixelsize + 1),
+                    nlinmin, nlinmax = (int(ymin), int(ymax + 2))
 
                     # Vertical range to be scanned.
-                    nlinrange = range(max(nlinmin - 1, 0),
-                                      min(nlinmax + 1, self.nbins[0] - 1))
+                    nlinrange = range(max(nlinmin, 0),
+                                      min(nlinmax, self.nbins[0]))
+
                     for nlin in nlinrange:
-                        self.mask[nlin, ncol] = self._pixel_weight(
-                            nlin, ncol, bot_eq[ii], top_eq[ii]
+                        self.mask[nlin, ncol] = self.pixel_weight(
+                            nlin, ncol, bot_eq[jj], top_eq[jj]
                         )
+
         return self.mask
 
     def _blades_coordinates(self):
@@ -221,91 +220,93 @@ class BladeMask:
 
         return blades
 
-    def _pixel_weight(self, nlin, ncol, bot, top):  # noqa: C901
-        """Calculate the intersection of the pixel with the blade.
+    def _pixel_area(self, key, x0, xf, ya, yb):
+        """Wrapper function to access pixel area dictionary.
 
-        Args:
-            ncol (int) : horizontal bottom left corner of the pixel
-            nlin (int) : vertical bottom left corner of the pixel
-            bot (tuple): coefficients of the linear equations which
-                        define the blade bottom edges;
-            top (tuple): coefficients of the linear equations which
-                        define the blade top edges;
-
-
-        Obs.: top/bottom refers to _blade_ edges;
-        '0'/'f' refers to _pixel's_ bottom/top edges, respectively;
-        'a' / 'b' refers to left/right pixel's coordinates (the interval),
-        respectively.
+        The letters in keys indicate the pixel borders crossed by
+        the edge's line, namely, T: top, B: bottom, L: left, R: right.
         """
-        # Pixel interval.
-        xa, y0 = ncol * self.pixelsize, nlin * self.pixelsize
-        xb, yf = xa + self.pixelsize, y0 + self.pixelsize
-        pixelarea = self.pixelsize * self.pixelsize
+        if key == 'TB':
+            return 0.5 * (x0 + xf)
 
-        # Weight is 1 if the pixel is fully inside the blade.
-        yatop, ybtop = self._linear(xa, *top), self._linear(xb, *top)
-        yabot, ybbot = self._linear(xa, *bot), self._linear(xb, *bot)
-        if yatop >= yf and ybtop >= yf and yabot <= y0 and ybbot <= y0:
-            return 1.0
+        if key == 'LR':
+            return 0.5 * (ya + yb)
 
-        # Analysis of edge cases.
+        if key == 'BL':
+            return 0.5 * x0 * ya
 
-        # Horizontal coordinate of the blade edge lines within the pixel
-        # interval, x = (y - b) / a.
-        #
-        # Blade top line related to pixel bottom/top.
-        x0top = (y0 - top[1]) / top[0]
-        xftop = (yf - top[1]) / top[0]
-        # Blade bottom line related to pixel bottom/top.
-        x0bot = (y0 - bot[1]) / bot[0]
-        xfbot = (yf - bot[1]) / bot[0]
+        if key == 'BR':
+            return 0.5 * x0 * yb
 
-        # Top line crosses pixel's upper edge
-        if xa <= xftop <= xb:
-            # and top line crosses left edge.
-            if y0 <= yatop <= yf:
-                return (1 - ((xftop - xa) * (yf - yatop) * 0.5) / pixelarea)
+        if key == 'TL':
+            return 1. - (0.5 * xf * (1 - ya))
 
-            # and top line crosses right edge.
-            if y0 <= ybtop <= yf:
-                return (1 - ((xb - xftop) * (yf - ybtop) * 0.5) / pixelarea)
+        if key == 'TR':
+            return 1. - 0.5 * (1 - xf) * (1 - yb)
 
-            # and top line crosses bottom edge.
-            if xa <= x0top <= xb:
-                # Ascending line.
-                if x0top <= xftop:
-                    return (((xb - x0top) + (xb - xftop))
-                            * 0.5 * self.pixelsize / pixelarea)
+    def pixel_weight(self, nlin, ncol, bot_eq, top_eq):
+        """Calculate the area of intersection of blade over the pixel.
 
-                # Descending line.
-                return (((x0top - xa) + (xftop - xa))
-                        * 0.5 * self.pixelsize / pixelarea)
+        The intersected area is the weight of the pixel to be considered.
 
-        # Bottom line crosses pixel's upper edge
-        if xa <= xfbot <= xb:
-            # and bottom line crosses left edge.
-            if y0 <= yabot <= yf:
-                return (yf - yabot) * (xfbot - xa) * 0.5 / pixelarea
+        Obs.: in this method, the pixel area is set to one and the
+        bottom-left corner of the pixel is considered as (0, 0) coordinate.
+        """
+        # Shift equations for current pixel position (set bottom-left
+        # coordinate to zero).
+        boteq = [bot_eq[0], bot_eq[1] + bot_eq[0] * ncol - nlin]
+        topeq = [top_eq[0], top_eq[1] + top_eq[0] * ncol - nlin]
 
-            # and top line crosses right edge.
-            if y0 <= ybbot <= yf:
-                return (xb - xfbot) * (yf - ybbot) * 0.5 / pixelarea
+        # Ordinates where the equations cross the interval.
+        yabot, ybbot = self._linear(0, *boteq), self._linear(1, *boteq)
+        yatop, ybtop = self._linear(0, *topeq), self._linear(1, *topeq)
 
-            # and bottom line crosses bottom edge.
-            if xa <= x0bot <= xb:
-                # Ascending line.
-                if x0bot <= xfbot:
-                    return (((xfbot - xa) + (x0bot - xb))
-                            * 0.5 * self.pixelsize / pixelarea)
+        # If corners are outside the blade.
+        if (yatop <= 0 and ybtop <= 0) or (yabot >= 1 and ybbot >= 1):
+            return 0
 
-                # Descending line.
-                return (((xb - xfbot) + (xb - x0bot))
-                        * 0.5 * self.pixelsize / pixelarea)
+        # If all corners are inside the blade.
+        if yabot < 0 and ybbot < 0 and yatop > 1 and ybtop > 1:
+            return 1.
 
-        return 0.0
+        # If bottom line crosses the pixel.
+        x0 = -yabot / (ybbot - yabot)
+        xf = (1 - yabot) / (ybbot - yabot)
+        if (0 <= x0 <= 1 or 0 <= xf <= 1 or
+            0 <= yabot <= 1 or 0 <= ybbot <= 1):
+            lineq = boteq
+            toparea = True
+        else:
+            # Top line crosses the pixel.
+            lineq = topeq
+            toparea = False
 
-    #
+        # Ordinates where the line crosses the interval [xa, xb].
+        ya = self._linear(0, *lineq)
+        yb = self._linear(1, *lineq)
+        # Abscissas where the line crosses the bottom/top of the
+        # pixel ordinates.
+        x0 = -ya / (yb - ya)
+        xf = (1. - ya) / (yb - ya)
+
+        # Classify the case according to crossing lines.
+        cross = ''
+        if 0 <= xf <= 1:
+            cross += 'T'
+        if 0 <= x0 <= 1:
+            cross += 'B'
+        if 0 <= ya <= 1:
+            cross += 'L'
+        if 0 <= yb <= 1:
+            cross += 'R'
+
+        # Calculate intersection area (blade over pixel).
+        area0 = self._pixel_area(cross[:2], x0, xf, ya, yb)
+        if toparea:
+            return 1.0 - area0
+        else:
+            return area0
+
     def _pixel_corner_weight(self, corners, blade_eqs):
         """Assign weights to the pixels at blade's corners."""
         for ic, corner in enumerate(corners):

xbpm_sim.py
@@ -20,7 +20,7 @@ where
 
 
 The default is non-interactive, meaning a sweep is made over the
-defined window.
+defined window. Length units are in mm; angles, in degrees.
 
 The parameters (to be defined in the parameter file) are:
 
@@ -208,8 +208,16 @@ def histogram_init(gprm):
     """
     beamhist = list()
     if gprm['distributionfile'] is not None:
-        beamhist.append(np.load(gprm['distributionfile']))
-        # with open(gprm['distributionfile']) as df:
+        hist = np.load(gprm['distributionfile'])
+        hlin, hcol = hist.shape
+        nlin, ncol = [int(x) for x in gprm['nbins'] + gprm['sweepinterval']]
+
+        if hlin < nlin or hcol < ncol:
+            print(">>>>> WARNING: beam distribution histogram"
+                  f" (shape = {hlin}, {hcol})is smaller than window box"
+                  f" (shape = {nlin}, {ncol}).\n"
+                  ">>>>> Superposition might be truncated at the borders.\n\n")
+        beamhist.append(hist)
     elif gprm['randomhist']:
         beamhist.append(gaussian_2d_samples(gprm, 0)[0])
     else:
@@ -267,7 +275,7 @@ def histogram_ring(gprm, mradius, sradius, center):
 
 
 def histogram_update(beamhist, gprm):
-    """Update all histogram-like distributions stored in beamhist."""
+    """Select method to update histogram, its type and number."""
     for ng in range(len(beamhist)):
         if gprm['distributionfile'] is not None:
             hist = histogram_shift(gprm['origbeam'],
@@ -286,7 +294,6 @@ def histogram_update(beamhist, gprm):
                 sradius=gh["sigmaradius"], center=gh["mean"]
             )
         beamhist[ng] = hist
-    return
 
 
 def gaussian_2d_analytic(gprm, ng):
@@ -358,6 +365,7 @@ def gaussian_2d_samples(gprm, idx):
 
 def histogram_shift(beamhist, mean, oldmean=(0, 0), pixelsize=0.2):
     """Just shift histogram by mean vector."""
+    # The image must extend to the whole box area.
     lin, col = beamhist.shape
     # New histogram image.
     newhist = np.zeros((lin, col))
@@ -447,6 +455,17 @@ def box_values_show(axval, flux, mean, pairpositions,
                       verticalalignment="top")
 
 
+def beam_over_mask(beam, mask):
+    """Superimpose beam over mask, considering their boundaries."""
+    blin, bcol = beam.shape
+    mlin, mcol = mask.shape
+    startlin = round((blin - mlin)/2)
+    endlin = startlin + mlin
+    startcol = round((bcol - mcol)/2)
+    endcol = startcol + mcol
+    return beam[startlin:endlin, startcol:endcol] * mask
+
+
 def image_show(count, beamhist, maskarray, bmp, gprm,  # noqa: ARG001
                axbeam, axblades, axval):
     """Add histograms and plot resulting image.
@@ -481,7 +500,7 @@ def image_show(count, beamhist, maskarray, bmp, gprm,  # noqa: ARG001
     # Apply the mask on the image created, so only the regions where
     # the distribution and the blades intersect are considered for the
     # measurements.
-    imgmasked = imgbeam * maskarray
+    imgmasked = beam_over_mask(imgbeam, maskarray)
     axblades.clear()
     imblades = axblades.imshow(imgmasked, origin="lower", extent=extent)
 
@@ -496,7 +515,8 @@ def image_show(count, beamhist, maskarray, bmp, gprm,  # noqa: ARG001
     return imbeam, imblades
 
 
-def measurement_record(mean, pairpositions, crosspositions, gprm):
+def measurement_record(mean, pairpositions, crosspositions, gprm,
+                       fluxes=None):
     """Write mean, crosspositions and pairpositions results to data file.
 
     Args:
@@ -504,6 +524,7 @@ def measurement_record(mean, pairpositions, crosspositions, gprm):
         pairpositions (array): pairwise measured positions;
         crosspositions (array): crossed-blades measured positions;
         gprm (dict): general parameters of the simulation.
+        fluxes (list) : values of flux on the blades.
     """
     outfilename = gprm['outfilename']
 
@@ -511,15 +532,19 @@ def measurement_record(mean, pairpositions, crosspositions, gprm):
     crossfile = f"{outfilename}-cross-00.dat"
     with open(crossfile, "a") as cf:
         dataline = (f"{mean[0]:.6f} {mean[1]:.6f}   "
-                    f"{crosspositions[0]:.6f} {crosspositions[1]:.6f}\n")
-        cf.write(dataline)
+                    f"{crosspositions[0]:.6f} {crosspositions[1]:.6f}")
+        if fluxes is not None:
+            dataline += "  " + " ".join([f"{flux:.6f}" for flux in fluxes])
+        cf.write(dataline + "\n")
 
     # Write out paired-blades position measurements.
     pairfile = f"{outfilename}-pair-00.dat"
     with open(pairfile, "a") as pf:
         dataline = (f"{mean[0]:.6f} {mean[1]:.6f}  "
-                    f"{pairpositions[0]:.6f} {pairpositions[1]:.6f}\n")
-        pf.write(dataline)
+                    f"{pairpositions[0]:.6f} {pairpositions[1]:.6f}")
+        if fluxes is not None:
+            dataline += "  " + "   ".join([f"{flux:.6f}" for flux in fluxes])
+        pf.write(dataline + "\n")
 
 
 def sweeping_points(gprm):
@@ -560,8 +585,9 @@ def parameters_write(gprm, pfile):
             f"# {datetime.now()}\n")
     pfile.write(head)
     for key, val in gprm.items():
-        # Skip if entry is a dictionary for gaussian distribution.
-        if isinstance(key, int):
+        # Skip if entry is a dictionary for gaussian distribution or
+        # a copy of the beam distribution.
+        if isinstance(key, int) or key == "origbeam":
             continue
         if key == 'cov':
             pfile.write(f"# {key:15} :  [{val[0]} {val[1]}]\n")
@@ -618,7 +644,8 @@ def sweep_make(fig, beamhist, imageshow, blades, bmp, gprm):
         imgbeam = shiftedbeam if gprm["ngauss"] == 1 else sum(beamhist)
         imshowbeam.set_data(imgbeam)
         #
-        imgmasked = imgbeam * blades.maskarray
+        # imgmasked = imgbeam * blades.maskarray
+        imgmasked = beam_over_mask(imgbeam, blades.maskarray)
         imshowblades.set_data(imgmasked)
 
         # Update measured data and show it.
@@ -628,7 +655,9 @@ def sweep_make(fig, beamhist, imageshow, blades, bmp, gprm):
                         crosspositions, ineigh)
 
         # Record values.
-        measurement_record(mean, crosspositions, pairpositions, gprm)
+        registerflux = fluxes if gprm['registerflux'] else None
+        measurement_record(mean, crosspositions, pairpositions, gprm,
+                           fluxes=registerflux)
 
         fig.canvas.draw_idle()
         plt.pause(0.1)
@@ -660,7 +689,8 @@ def parameters_read(parfilename, distributionfile):
                 v1 = float(re.sub(r'[\[\,]', '', val[0]))
                 v2 = float(re.sub(r'[\]\,]', '', val[1]))
                 prm[key] = np.array([v1, v2])
-            elif key in ['addring', 'histupdate', 'randomhist']:
+            elif key in ['addring', 'histupdate',
+                         'randomhist', 'registerflux']:
                 prm[key] = False if val[0] == 'False' else True
             elif key in ['ngauss', 'nsweeps', 'nsample']:
                 prm[key] = int(float(val[0]))
@@ -764,6 +794,17 @@ def main():
 
     # Create blades array, a 'mask'.
     blades = BladeMask(gprm)
+
+    # DEBUG
+    # print("\n>>>>> (MAIN) gprm :")
+    # for key, val in gprm.items():
+    #     print(f" {key} = {val}")
+    # print("\n\n")
+
+    # plt.imshow(blades.maskarray)
+    # plt.show()
+    # sys.exit(0)
+    # DEBUG
 
     # Initialize beam position calculation methods.
     bmp = BmP(blades.bladescoordinates, gprm)