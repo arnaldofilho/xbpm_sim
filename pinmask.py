72b26a28139c86f922e86205e372900aa4f48966
Arnaldo Gomes de Oliveira-Filho <arnaldo.filho@lnls.br>
2025-10-29 08:56:47 -0300

    pinmask:  - pind diodes may have a square size (given by 'pinsize);  - number of pixels of a pin set to an odd number, for the sake of symmetry relative to its coordinates;  - added the possibility of horizontally central sets of pins.

--- pinmask.py
+++ pinmask.py
@@ -7,7 +7,6 @@ vertical directions respectively.
 
 import numpy as np
 # import matplotlib.pyplot as plt
-from copy import deepcopy
 
 
 class PinMask:
@@ -19,24 +18,27 @@ class PinMask:
 
     The standard values are based on real dimensions of the structures, in mm.
     """
-    def __init__(self, gprm):
+    def __init__(self, prm):
         """Define the main parameters of the mask."""
-        self.nbins         = gprm["nbins"]
-        self.ndots         = gprm["ndots"]
-        self.windowsize    = gprm["windowsize"]
-        self.pixelsize     = gprm["pixelsize"]
-        self.phi           = self._degtorad(gprm["phi"])
-        self.bladelength   = gprm["bladelength"]
-        self.pinthickness  = gprm["pinthickness"]
-        self.corneroffset  = gprm["corneroffset"]
-        self.coordinates   = self.pin_coordinates_calc()
+        self.nbins        = prm["nbins"]
+        self.ndots        = prm["ndots"]
+        self.windowsize   = prm["windowsize"]
+        self.pixelsize    = prm["pixelsize"]
+        self.bladelength  = prm["bladelength"]
+        self.pinsize      = prm["pinsize"]
+        self.centerpins   = prm["centerpins"]
+        self.wcenter      = self.windowsize / 2
+        self.phi          = self._degtorad(prm["phi"])
+        self.corneroffset = np.array([prm["corneroffset"], 0.0])
+        self.coordinates  = self.pin_coordinates_calc()
         self.mask_array()
 
     def mask_array(self):
-        """Create a mask to weight the intersection of pin diodes and radiation distribution.
+        """Mask to weight the intersection of pin diodes and  distribution.
 
-        The mask values are 1 where the pin diodes are defined (dots), 0 elsewhere.
-        
+        The mask values are 1 where the pin diodes are defined (dots),
+        0 elsewhere.
+
         Warning: the usual indexing of an array is from top to bottom, but
         the coodrinates were chosen accordingly to the Cartesian systems,
         so the current indexing is reversed.
@@ -46,59 +48,54 @@ class PinMask:
         coordinates_array = list()
 
         # Scale to array units.
-        pxnorm = 1. / self.pixelsize
-
-        # DEBUG
-        print(f"\n\n>>> (MASK ARRAY) nbins      = {self.nbins}\n")
-        print(f">>> (MASK ARRAY) windowsize = {self.windowsize}\n")
-        # DEBUG
+        # pxnorm = 1. / self.pixelsize
+        nsize = int(self.pinsize / self.pixelsize)
+        # Pin size in number of pixels is odd, for symmetry.
+        if nsize % 2 == 0:
+            nsize += 1
+        halfnsize = int(nsize / 2)
 
         # Run through dot sets to assign weights to pixels.
-        for idx, pinset in enumerate(self.coordinates):
-                
-            # DEBUG
-            print(f">>> (MASK ARRAY) pinset {idx} = \n{pinset}\n")
-            # DEBUG
-
-            for idd, sdot in enumerate(pinset):
-                lin = round(self.nbins[1] * (1 - sdot[1] / self.windowsize[1]))
-                col = round(sdot[0] / self.windowsize[0] * self.nbins[0])
-                coordinates_array.append([lin, col])
-
-                # DEBUG
-                print(f">>> (MASK ARRAY) \nlin ({idd}): {sdot[1]} -> {lin}\n")
-                print(f">>> (MASK ARRAY) \ncol ({idd}): {sdot[0]} -> {col}\n")
-                # DEBUG
-
-                for c in range():
-                    self.mask[lin, col] = 1
-
+        for pinc in self.coordinates:
+            # Array indices from window coordinates.
+            lin = round(self.nbins[1] *
+                        (pinc[1] + self.wcenter[1])/ self.windowsize[1])
+            col = round(self.nbins[0] *
+                        (pinc[0] + self.wcenter[0])/ self.windowsize[0])
+            coordinates_array.append([lin, col])
+            # Set mask.
+            for ll in range(lin - halfnsize, lin + halfnsize + 1):
+                for cc in range(col - halfnsize, col + halfnsize + 1):
+                    self.mask[ll, cc] = 1
         self.coordinates_array = np.array(coordinates_array)
 
     def pin_coordinates_calc(self):
-        """Create a list of the coordinates of the dots in each blade.
+        """Create a list of the coordinates of the dots in each set.
 
-        Each element of the list is an array with the coordinates of the
-        dots in a set, as many as defined by ndots.
+        Each element of the list is an array with the (x, y) coordinates
+        of the dots in a set, as many as defined by ndots * 4 (sets at the
+        corners) or ndots * 6 (corners + horizontal center).
 
-        Each blade is initially created at the bottom left corner, then it is 
+        Each set is initially created at the bottom left corner, then it is
         mirrored to other corners, taking into account the corner offset.
 
         At this stage, the coordinates and measures are set in mm. They are
         transformed onto array indices afterwards.
 
-        phi is the azimuthal angle (in rad) of the blades; not to be mistaken
-        with the theta angle, relative to the cut edge of the blades,
-        upon which the x-ray is incident.
+        phi is the azimuthal angle (in rad) of the radial set.
+
+        Args:
+            self
 
         Returns:
-            blades (list): each blades' corners' coordinates.
+            pin_set (numpy_array): each pin's coordinates.
         """
-        # Versor in phi direction.       
+        # Versor in phi direction.
         phivec = np.array([np.cos(self.phi), np.sin(self.phi)])
 
         # Pin coordinates in bottom left set.
-        # The set is displaced from bottom left corner according to corner offset.
+        # The set is displaced from bottom left corner according to
+        # corner offset.
         setvec = phivec * self.bladelength
         set_bottom_left = [
             (1.0 - ii / self.ndots) * setvec + self.corneroffset
@@ -110,7 +107,7 @@ class PinMask:
             [self.windowsize[0] - xx , yy]
             for xx, yy in set_bottom_left
             ]
-        
+
         set_top_left = [
             [xx, self.windowsize[1] - yy]
             for xx, yy in set_bottom_left
@@ -120,15 +117,31 @@ class PinMask:
             [xx , self.windowsize[1] - yy]
             for xx, yy in set_bottom_right
             ]
-        
-        # DEBUG
-        pinset = np.array([set_top_left, set_top_right, set_bottom_right, set_bottom_left])
-        print(f"\n\n ##### PIN COORDINATES #####\n {pinset}")
-        # DEBUG
 
-        # From top left, clockwise.
-        return np.array([set_top_left, set_top_right, set_bottom_right, set_bottom_left])
+        pin_set = (set_top_left + set_top_right +
+                   set_bottom_right + set_bottom_left)
 
+        # Include pins at central vertical line.
+        if self.centerpins:
+            bladecenterlength = self.bladelength * np.sin(self.phi)
+            set_top_center = [
+                [self.wcenter[0],
+                 (1.0 - ii / self.ndots) * bladecenterlength]
+                for ii in range(self.ndots)
+                ]
+
+            set_bottom_center = [
+                [xx, self.windowsize[1] - yy]
+                for xx, yy in set_top_center
+                ]
+            pin_set = (set_top_left + set_top_center + set_top_right +
+                       set_bottom_right + set_bottom_center + set_bottom_left)
+
+        # Shift center to the window's center.
+        pin_set = np.array(pin_set)
+        for ps in pin_set:
+            ps -= self.wcenter
+        return np.array(pin_set)
 
     def _degtorad(self, phi):
         """Convert angle from degrees to radians."""
